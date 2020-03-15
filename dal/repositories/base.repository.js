336e84a4eafe051a0dd7a37724f6dad036c1dcb6
danielgtdm <daniel.gtmd@gmail.com>
2020-03-15 20:56:20 -0300

    prueba 13 dde tipo de dato

--- dal/repositories/base.repository.js
+++ dal/repositories/base.repository.js
@@ -31,7 +31,7 @@ class BaseRepository {
   update(id, entity) {
     delete entity.createdAt;
     delete entity.updatedAt;
-    console.log("Objeto a guadar en la BD: " + entity);
+    console.log("Objeto a guadar en la BD: " + entity.h0830);
     return this._db[this.entity].update(entity, { where: { id } });
   }
 
