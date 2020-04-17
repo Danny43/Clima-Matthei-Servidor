0ef19b2c9c4ea100b7f54a42c18a9c0b04577c88
danielgtdm <daniel.gtmd@gmail.com>
2020-04-17 17:02:15 -0400

    correccion

--- api/routes/index.js
+++ api/routes/index.js
@@ -38,9 +38,9 @@ module.exports = function({
   apiRoute.use("/registro", RegistroRoutes);
   apiRoute.use("/registro-fecha", RegistroFechaRoutes);
   apiRoute.use("/usuario", UsuarioRoutes);
-  apiRoute.use("/excel", ExcelRoutes);
+  routerExcel.use("/excel", ExcelRoutes);
   router.use("/api", apiRoute);
- // router.use("/api", routerExcel);
+  router.use("/api", routerExcel);
 
   return router;
 };
