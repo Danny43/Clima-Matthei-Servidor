16fdcc1918809282d634877c0d886d114a61197f
danielgtdm <daniel.gtmd@gmail.com>
2020-08-23 07:02:07 -0400

    add month july

--- api/controllers/registro.controller.js
+++ api/controllers/registro.controller.js
@@ -26,8 +26,8 @@ let ConvertFEBRERO = require("../converts/convertFebrero");
 let ConvertMARZO = require("../converts/convertMarzo");
 let ConvertABRIL = require('../converts/convertAbril');
 let ConvertMAYO = require('../converts/convertMayo');
-let ConvertJunio = require('../converts/convertJunio');
-const ConvertJUNIO = require("../converts/convertJunio");
+let ConvertJUNIO = require("../converts/convertJunio");
+let convertJULIO = require('../converts/convertJulio');
 
 class RegistroController {
   constructor({
@@ -555,6 +555,49 @@ class RegistroController {
       console.log('TAMANO DEL REGISTRO DE JUNIO: ' + junio.length);
       for (let index = 0; index < junio.length; index++) {
         const registroExcel = junio[index];
+        const fecha = registroExcel.fecha;
+        var r2 = new Registro2Dto();
+
+        var fechaPicker = new Date(fecha)
+        var fechaString = String(fechaPicker.getFullYear()) + "-";
+        if (fechaPicker.getMonth() + 1 < 10) {
+          fechaString += "0" + String(fechaPicker.getMonth() + 1) + "-";
+        } else {
+          fechaString += String(fechaPicker.getMonth() + 1) + "-";
+        }
+        if (fechaPicker.getDate() < 10) {
+          fechaString += "0" + String(fechaPicker.getDate()) + "T";
+        } else {
+          fechaString += String(fechaPicker.getDate()) + "T";
+        }
+        fechaString += "00:00:00Z";
+        var fechaBusqueda = new Date(fechaString);
+
+        registroExcel.fecha = fechaBusqueda;
+
+        let registroF = await this._registroService.getbyFecha(fechaBusqueda);
+
+        let token = req.headers.authorization.split(' ')[1];
+        let payload = jwt.verify(token, 'secretKey');
+        const idUser = payload.subject;
+        const IPUser = req.header('x-forwarded-for') || req.connection.remoteAddress;
+        if (registroF == null) { //REGISTRAR FECHA SI NO EXISTE
+          console.log('CREAR FECHA: ' + registroExcel.fecha);
+          await this.registrarDesdeExcel(registroExcel, idUser, IPUser);
+        }
+
+        if (registroF != null) { //ACTUALIZAR FECHA SI YA EXISTE
+          console.log('ACTUALIZAR FECHA: ' + registroExcel.fecha);
+          await this.actualizarDesdeExcel(registroF, registroExcel, idUser, IPUser);
+
+        }
+      }
+
+      var ju = new ConvertJULIO(filename);
+      var julio = await ju.getRegistros();
+      console.log('TAMANO DEL REGISTRO DE JULIO: ' + julio.length);
+      for (let index = 0; index < julio.length; index++) {
+        const registroExcel = julio[index];
         const fecha = registroExcel.fecha;
         var r2 = new Registro2Dto();
 
